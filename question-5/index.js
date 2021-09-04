53cf72587ca140ee875c8178b43b55faf97a93ca
ANUJ KUMAR YADAV <65344146+anuj-kumary@users.noreply.github.com>
2021-09-04 13:03:54 +0000

    feat : compare highest marks

--- question-5/index.js
+++ question-5/index.js
@@ -1,2 +1,57 @@
 const readlineSync = require('readline-sync');
 const chalk = require('chalk');
+
+const listOfStudent = [];
+
+for (let i = 0; i < 5; i++) {
+  let name = readlineSync.question("Enter Your Name: ")
+  let unitTestMark = readlineSync.question("Enter Your Unit Test Mark: ")
+  let preliumMarks = readlineSync.question("Enter Your Prelium Marks: ")
+  let finalMarks = readlineSync.question("Enter Your Final Marks: ")
+  console.log(chalk.blue("--------------------------------------------"))
+  console.log(chalk.blue("--------------------------------------------"))
+
+  const studentObj = {
+    name: name,
+    unitMarks: unitTestMark,
+    preliumTest: preliumMarks,
+    finalTest: finalMarks
+  }
+
+  listOfStudent.push(studentObj);
+}
+
+function comapareMarks(listOfStudent) {
+  if(Number(listOfStudent[0].finalTest) > Number(listOfStudent[1].finalTest) &&
+    Number(listOfStudent[0].finalTest) > Number(listOfStudent[2].finalTest) &&
+    Number(listOfStudent[0].finalTest) > Number(listOfStudent[3].finalTest) &&
+    Number(listOfStudent[0].finalTest) > Number(listOfStudent[4].finalTest) 
+  ){
+    console.log(listOfStudent[0].name + " has the highest marks")
+  }
+  else if(Number(listOfStudent[1].finalTest) > Number(listOfStudent[0].finalTest) &&
+    Number(listOfStudent[1].finalTest) > Number(listOfStudent[2].finalTest) &&
+    Number(listOfStudent[1].finalTest) > Number(listOfStudent[3].finalTest) &&
+    Number(listOfStudent[1].finalTest) > Number(listOfStudent[4].finalTest) )
+  {
+    console.log(listOfStudent[1].name + " has the highest marks")
+  }
+  else if(Number(listOfStudent[2].finalTest) > Number(listOfStudent[0].finalTest) &&
+    Number(listOfStudent[2].finalTest) > Number(listOfStudent[1].finalTest) &&
+    Number(listOfStudent[2].finalTest) > Number(listOfStudent[3].finalTest) &&
+    Number(listOfStudent[2].finalTest) > Number(listOfStudent[4].finalTest))
+    {
+      console.log(listOfStudent[2].name + " has the highest marks")
+  }
+  else if(Number(listOfStudent[3].finalTest) > Number(listOfStudent[0].finalTest) &&
+    Number(listOfStudent[3].finalTest) > Number(listOfStudent[1].finalTest) &&
+    Number(listOfStudent[3].finalTest) > Number(listOfStudent[2].finalTest) &&
+    Number(listOfStudent[3].finalTest) > Number(listOfStudent[4].finalTest))
+    {
+      console.log(listOfStudent[3].name + " has the highest marks")
+  }else{
+    console.log(listOfStudent[4].name + " has the highest marks")
+  }
+}
+
+comapareMarks(listOfStudent)
