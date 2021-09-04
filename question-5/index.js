dece517be44bd352a01037a77a4c7a0ea15679dc
ANUJ KUMAR YADAV <65344146+anuj-kumary@users.noreply.github.com>
2021-09-04 13:47:03 +0000

    feat : compare highest marks and average marks

--- question-5/index.js
+++ question-5/index.js
@@ -1,57 +1,129 @@
-const readlineSync = require('readline-sync');
-const chalk = require('chalk');
+const readlineSync = require("readline-sync");
+const chalk = require("chalk");
 
-const listOfStudent = [];
+console.log(chalk.yellow("Student Mark Database"));
+console.log('------------------------------');
+console.log('------------------------------');
 
-for (let i = 0; i < 5; i++) {
-  let name = readlineSync.question("Enter Your Name: ")
-  let unitTestMark = readlineSync.question("Enter Your Unit Test Mark: ")
-  let preliumMarks = readlineSync.question("Enter Your Prelium Marks: ")
-  let finalMarks = readlineSync.question("Enter Your Final Marks: ")
-  console.log(chalk.blue("--------------------------------------------"))
-  console.log(chalk.blue("--------------------------------------------"))
+let studentList = [];
 
-  const studentObj = {
-    name: name,
-    unitMarks: unitTestMark,
-    preliumTest: preliumMarks,
-    finalTest: finalMarks
+for(let i = 0; i < 2; i++){
+    let name = readlineSync.question("Enter Your Name ");
+    let unitTest = readlineSync.question("Enter Your Unit Test Marks ");
+    let preliumTest = readlineSync.question("Enter the Prelium Test Marks ");
+    let finalTest = readlineSync.question("Enter The Final Test Marks ");
+
+  let studentObj = {
+    name : name,
+    unitMarks : unitTest,
+    preliumMarks : preliumTest,
+    finalMarks : finalTest
   }
-
-  listOfStudent.push(studentObj);
+  studentList.push(studentObj);
 }
-
-function comapareMarks(listOfStudent) {
-  if(Number(listOfStudent[0].finalTest) > Number(listOfStudent[1].finalTest) &&
-    Number(listOfStudent[0].finalTest) > Number(listOfStudent[2].finalTest) &&
-    Number(listOfStudent[0].finalTest) > Number(listOfStudent[3].finalTest) &&
-    Number(listOfStudent[0].finalTest) > Number(listOfStudent[4].finalTest) 
-  ){
-    console.log(listOfStudent[0].name + " has the highest marks")
-  }
-  else if(Number(listOfStudent[1].finalTest) > Number(listOfStudent[0].finalTest) &&
-    Number(listOfStudent[1].finalTest) > Number(listOfStudent[2].finalTest) &&
-    Number(listOfStudent[1].finalTest) > Number(listOfStudent[3].finalTest) &&
-    Number(listOfStudent[1].finalTest) > Number(listOfStudent[4].finalTest) )
+  function comapareValues(studentList){
+  
+  if(Number(studentList[0].finalMarks) > Number(studentList[1].finalMarks) )
   {
-    console.log(listOfStudent[1].name + " has the highest marks")
+    console.log(studentList[0].name + " Has a higher Marks")
   }
-  else if(Number(listOfStudent[2].finalTest) > Number(listOfStudent[0].finalTest) &&
-    Number(listOfStudent[2].finalTest) > Number(listOfStudent[1].finalTest) &&
-    Number(listOfStudent[2].finalTest) > Number(listOfStudent[3].finalTest) &&
-    Number(listOfStudent[2].finalTest) > Number(listOfStudent[4].finalTest))
-    {
-      console.log(listOfStudent[2].name + " has the highest marks")
+  else if(Number(studentList[1].finalMarks) > Number(studentList[0].finalMarks))
+  {
+    console.log(studentList[1].name + " Has a higher Marks")
   }
-  else if(Number(listOfStudent[3].finalTest) > Number(listOfStudent[0].finalTest) &&
-    Number(listOfStudent[3].finalTest) > Number(listOfStudent[1].finalTest) &&
-    Number(listOfStudent[3].finalTest) > Number(listOfStudent[2].finalTest) &&
-    Number(listOfStudent[3].finalTest) > Number(listOfStudent[4].finalTest))
-    {
-      console.log(listOfStudent[3].name + " has the highest marks")
-  }else{
-    console.log(listOfStudent[4].name + " has the highest marks")
+  else
+  {
+     console.log("Sorry Draw");
   }
 }
 
-comapareMarks(listOfStudent)
+  function studentAverage(studentList){
+
+    let averageSum = parseInt(studentList[0].finalMarks) + parseInt(studentList[1].finalMarks);
+    console.log(averageSum)
+    let averages = averageSum / 2;
+    console.log(averages)
+  }
+
+comapareValues(studentList);
+studentAverage(studentList)const readlineSync = require("readline-sync");
+const chalk = require("chalk");
+
+console.log(chalk.yellow("Student Mark Database"));
+console.log('------------------------------');
+console.log('------------------------------');
+
+let studentList = [];
+
+for(let i = 0; i < 5; i++){
+    let name = readlineSync.question("Enter Your Name ");
+    let unitTest = readlineSync.question("Enter Your Unit Test Marks ");
+    let preliumTest = readlineSync.question("Enter the Prelium Test Marks ");
+    let finalTest = readlineSync.question("Enter The Final Test Marks ");
+
+  let studentObj = {
+    name : name,
+    unitMarks : unitTest,
+    preliumMarks : preliumTest,
+    finalMarks : finalTest
+  }
+  studentList.push(studentObj);
+}
+  function comapareValues(studentList){
+  
+  if( Number(studentList[0].finalMarks) > Number(studentList[1].finalMarks) && 
+      Number(studentList[0].finalMarks) > Number(studentList[2].finalMarks) &&
+      Number(studentList[0].finalMarks) > Number(studentList[3].finalMarks) &&
+      Number(studentList[0].finalMarks) > Number(studentList[4].finalMarks)
+    )
+  {
+    console.log(studentList[0].name + " Has a higher Marks")
+  }
+  else if(Number(studentList[1].finalMarks) > Number(studentList[0].finalMarks) &&
+          Number(studentList[1].finalMarks) > Number(studentList[2].finalMarks) &&
+          Number(studentList[1].finalMarks) > Number(studentList[3].finalMarks) &&
+          Number(studentList[1].finalMarks) > Number(studentList[4].finalMarks)
+  )
+  {
+    console.log(studentList[1].name + " Has a higher Marks")
+  }
+  else if(Number(studentList[2].finalMarks) > Number(studentList[0].finalMarks) &&
+          Number(studentList[2].finalMarks) > Number(studentList[1].finalMarks) &&
+          Number(studentList[2].finalMarks) > Number(studentList[3].finalMarks) &&
+          Number(studentList[2].finalMarks) > Number(studentList[4].finalMarks)
+  )
+  {
+    console.log(studentList[2].name + " Has a higher Marks")
+  }
+  else if(Number(studentList[3].finalMarks) > Number(studentList[0].finalMarks) &&
+          Number(studentList[3].finalMarks) > Number(studentList[1].finalMarks) &&
+          Number(studentList[3].finalMarks) > Number(studentList[2].finalMarks) &&
+          Number(studentList[3].finalMarks) > Number(studentList[4].finalMarks)
+  )
+  {
+    console.log(studentList[3].name + " Has a higher Marks")
+  }
+  else if(Number(studentList[4].finalMarks) > Number(studentList[0].finalMarks) &&
+          Number(studentList[4].finalMarks) > Number(studentList[1].finalMarks) &&
+          Number(studentList[4].finalMarks) > Number(studentList[2].finalMarks) &&
+          Number(studentList[4].finalMarks) > Number(studentList[3].finalMarks)
+  )
+  {
+    console.log(studentList[4].name + " Has a higher Marks")
+  }
+  else
+  {
+     console.log("Sorry Draw");
+  }
+}
+
+  function studentAverage(studentList){
+
+    let averageSum = parseInt(studentList[0].finalMarks) + parseInt(studentList[1].finalMarks) + parseInt(studentList[2].finalMarks) + parseInt(studentList[3].finalMarks) + parseInt(studentList[4].finalMarks) ;
+    console.log(averageSum)
+    let averages = averageSum / 5;
+    console.log(averages)
+  }
+
+comapareValues(studentList);
+studentAverage(studentList)
