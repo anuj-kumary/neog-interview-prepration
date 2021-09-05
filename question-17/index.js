bca501d761444915ab210d8895ee12d412f74510
ANUJ KUMAR YADAV <65344146+anuj-kumary@users.noreply.github.com>
2021-09-05 16:22:02 +0000

    feat : fake news or true

--- question-17/index.js
+++ question-17/index.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+const readlineSync = require('readline-sync');
+
+
+function detectNews(questions, answers) {
+
+  let answer = readlineSync.question(questions);
+  if (answer === answers) {
+    console.log('Don"t believe things on FB , Whatsapp and telegram');
+  } else {
+    console.log('This is true news');
+  }
+
+}
+detectNews("Don't believe things on FB and Whatsapp ", "FB");
+detectNews("Don't believe things on FB and Whatsapp ", "Whatsapp");
+detectNews("Don't believe things on FB and Whatsapp ", "telegram");
