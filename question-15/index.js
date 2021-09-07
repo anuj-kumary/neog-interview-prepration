d1fda7f79c675396ac89de23905bfabb50ec398f
ANUJ KUMAR YADAV <65344146+anuj-kumary@users.noreply.github.com>
2021-09-07 15:12:40 +0000

    feat : persom with more power

--- question-15/index.js
+++ question-15/index.js
@@ -1,1 +1,20 @@
-const chalk = require('chalk');
+const obj1 = {
+  name : "Ram",
+  age : "Dwapar",
+  power : 2500
+}
+
+const obj2= {
+  name : "Krishna",
+  age : "Treta",
+  power : 2325
+}
+
+function morePower(obj1 , obj2){
+  if(obj1.power > obj2.power ){
+    console.log(obj1.name + " have a more power ")
+  }else{
+     console.log(obj2.name + " have a more power ")
+  }
+}
+morePower(obj1 , obj2)
